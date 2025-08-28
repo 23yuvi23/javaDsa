d316af137f8e61126eaa473150686e8fe4b63dc5
23yuvi23 <yogeshpaliwal9493@gmail.com>
2025-08-28 12:52:26 +0530

    binary and prime

--- functions and methods/primeOrNot.java
+++ functions and methods/primeOrNot.java
@@ -1,3 +1,32 @@
+
+
 public class primeOrNot {
+
+    public static boolean  isprime(int num){
+      for (int i=2;i<=Math.sqrt(num);i++){
+        if(num==2){
+            return true;
+        }
+       if(num%i==0){
+        return false;
+       }
+      }
+      return true;
+    }
+
+    public static void allFactorialNumbers(int n){
+        for(int  i =0;i<=n;i++){
+            if(isprime(i)){
+                System.out.print(i+ " " );
+    }
+}
+    }
+   
+    public static void main(String[] args) {
+        // int  i = isprime (23);
+        allFactorialNumbers(23);
+
+        
+    }
     
 }
