5c817992b9850d54baa60f9f599808af28f2aed2
23yuvi23 <yogeshpaliwal9493@gmail.com>
2025-08-29 10:05:26 +0530

    advancepattern

---
+++ patternAdvance/HollowRectangle.java
@@ -0,0 +1,24 @@
+public class hollowRectangle{
+
+    public static void rectangle(int totRows, int totCols) {
+        //outerloop
+        for(int i = 1;i<=totRows;i++){
+            //inner loop
+            for(int j = 1 ; j<=totCols ; j++){
+                if (i==1 || i ==totRows || j ==1 || j==totCols){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    } 
+    public static void main(String[] args) {
+        rectangle(10, 7);
+        
+    }
+
+
+    
+}
