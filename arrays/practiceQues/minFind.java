2664fc1af276b17ef0e6a861f541e27d910b504c
23yuvi23 <yogeshpaliwal9493@gmail.com>
2025-10-03 15:31:27 +0530

    arrays

--- arrays/practiceQues/minFind.java
+++ arrays/practiceQues/minFind.java
@@ -1,33 +1,45 @@
+//leetcode ques no 33 
+
 public class minFind {
+    public static int SearchRotated(int[] nums,int target){
+        int low = 0;
+        int high = nums.length-1;
 
-    public static int binarySearch(int[] nums ,int target){
+        while (low<=high){
+            int mid = (low+high)/2;
 
-        int min = 
-        for(int i=0;i<nums.length;i++){
+            if(nums[mid]== target) return mid;
 
-        }
+            //leftsorted
+            if(nums[low]<=nums[mid]){
+                //array lies in sorted half
+                if(nums[low]<=target && target <= nums[mid]){
+                    high = mid -1;
+                }
+                //not lies in sorted half 
+                else {
+                    low = mid+1;
+                }
 
-    }
-
-    public static int binarySearch(int[] nums , int min , int max , int target){
-
-    }
-
-    public static int minSearch(int[] nums){
-        int left =0;
-        int right= nums.length-1;
-
-        while(left<right){
-            int mid = left+(right-left)/2;
-            if(mid>0 && nums[mid-1]>nums[mid]){
-                return mid;
+            //rightsorted
+            }else {
+                //array lies in sorted half
+            if(nums[mid]<=target && nums[high]>=target){
+               low = mid +1 ;
+            } 
+            //array not lies in the sorted half 
+            else {
+                high = mid - 1;
             }
-            else if()
         }
     }
+        return -1;
+}  
     public static void main(String[] args) {
         
         int[] nums = {4,5,6,7,0,1,2};
-        int target = 1;
+        int target = 3;
+
+        System.out.println(SearchRotated(nums, target));
     }
 }
